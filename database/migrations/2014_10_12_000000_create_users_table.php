86d3c800384dec54b3202c5482608a9d66e019d7
Freddy Perez <freddy.perez.trabajos@gmail.com>
2020-12-26 12:30:54 -0300

    correccion error img user default

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('lastname');
             $table->string('email')->unique();
             $table->string('tel');
-            $table->string('img');
+            $table->string('img')->default('placeholder.jpg');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
