205eeba8b438dc88819d55aa2f9aaaa7c7a2396e
Freddy Perez <freddy.perez.trabajos@gmail.com>
2021-02-09 19:33:41 -0300

    actualizacion de directorios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             BannerHomeSeeder::class,
+            ProductoSeeder::class,
             PlcSeeder::class
         ]);
     }
---
+++ database/seeders/ProductoSeeder.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('productos')->insert([[
+            'categoria_id' => 0,
+            'marca_id' => 0,
+            'name' => 'producto ejemplo',
+            'description' => 'descripcion ejemplo',
+            'price' => 10000,
+            'oldprice' => 15000,
+            'enabled' => true,
+            'stock' => 20,
+            'img' => '1608002760.png'
+        ],
+            [
+                'categoria_id' => 0,
+                'marca_id' => 0,
+                'name' => 'producto ejemplo',
+                'description' => 'descripcion ejemplo',
+                'price' => 10000,
+                'oldprice' => 15000,
+                'enabled' => true,
+                'stock' => 20,
+                'img' => '1608002760.png'
+            ],
+            [
+                'categoria_id' => 0,
+                'marca_id' => 0,
+                'name' => 'producto ejemplo',
+                'description' => 'descripcion ejemplo',
+                'price' => 10000,
+                'oldprice' => 15000,
+                'enabled' => true,
+                'stock' => 20,
+                'img' => '1608002760.png'
+            ],
+            [
+                'categoria_id' => 0,
+                'marca_id' => 0,
+                'name' => 'producto ejemplo',
+                'description' => 'descripcion ejemplo',
+                'price' => 10000,
+                'oldprice' => 15000,
+                'enabled' => true,
+                'stock' => 20,
+                'img' => '1608002760.png'
+            ],
+            [
+                'categoria_id' => 0,
+                'marca_id' => 0,
+                'name' => 'producto ejemplo',
+                'description' => 'descripcion ejemplo',
+                'price' => 10000,
+                'oldprice' => 15000,
+                'enabled' => true,
+                'stock' => 20,
+                'img' => '1608002760.png'
+            ],
+            [
+                'categoria_id' => 0,
+                'marca_id' => 0,
+                'name' => 'producto ejemplo',
+                'description' => 'descripcion ejemplo',
+                'price' => 10000,
+                'oldprice' => 15000,
+                'enabled' => true,
+                'stock' => 20,
+                'img' => '1608002760.png'
+            ],[
+                'categoria_id' => 0,
+                'marca_id' => 0,
+                'name' => 'producto ejemplo',
+                'description' => 'descripcion ejemplo',
+                'price' => 10000,
+                'oldprice' => 15000,
+                'enabled' => true,
+                'stock' => 20,
+                'img' => '1608002760.png'
+            ]]);
+    }
+}
