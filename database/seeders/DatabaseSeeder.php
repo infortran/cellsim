e93fa00338ce0adbbc4ed8aad2437853ecb22357
Freddy Perez <freddy.perez.trabajos@gmail.com>
2021-02-09 18:12:53 -0300

    seeder bug

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            //UserSeeder::class,
-            //BannerHomeSeeder::class,
+            UserSeeder::class,
+            BannerHomeSeeder::class,
             PlcSeeder::class
         ]);
     }
