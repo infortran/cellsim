d9b205402b242ee24906cdd71b9d5d16077092db
Freddy Perez <freddy.perez.trabajos@gmail.com>
2021-02-09 17:20:33 -0300

    BUG PLC 1.1

--- resources/views/templates/product-list-component.blade.php
+++ resources/views/templates/product-list-component.blade.php
@@ -27,6 +27,7 @@
             <div class="tab-content" id="Bpills-tabContent">
                 <div class="tab-pane fade pt-2 show active" id="Bpills-one-example1" role="tabpanel" aria-labelledby="Bpills-one-example1-tab">
                     <ul class="row list-unstyled products-group no-gutters mb-0">
+                        @isset($plc)
                         @foreach($plc->categoria->productos as $producto)
                             <li class="col-6 col-md-5 col-lg-3 product-item mb-4">
                                 <div class="product-item__outer h-100" style="width: 100%">
@@ -60,6 +61,7 @@
                                 </div>
                             </li>
                         @endforeach
+                        @endisset
                     </ul>
                 </div>
             </div>
